0495efcf49422c7a503f4b96c745c4e83bc6e837
Nithin Guvvala <nithin@Nithins-Air.ht.home>
2023-04-26 17:37:41 -0400

    Final

---
+++ IOS Application/Controller/ConversationController.swift
@@ -0,0 +1,28 @@
+//
+//  ConversationController.swift
+//  FinalProject
+//
+//  Created by Nithin Guvvala on 12/11/22.
+//
+
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Messages"
+    }
+}
---
+++ IOS Application/Model/Tweet.swift
@@ -0,0 +1,40 @@
+//
+//  Tweet.swift
+//  FinalProject
+//
+//  Created by Nithin Guvvala on 12/14/22.
+//
+
+
+import Foundation
+
+struct Tweet {
+    let caption: String
+    let tweetID: String
+    var likes: Int
+    var timestamp: Date!
+    let retweetCount: Int
+    var user: User
+    var didLike = false
+    var replyingTo: String?
+    
+    var isReply: Bool { return replyingTo != nil }
+    
+    init(user: User, tweetID: String, dictionary: [String: Any]) {
+        self.tweetID = tweetID
+        self.user = user
+        
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.likes = dictionary["likes"] as? Int ?? 0
+        self.retweetCount = dictionary["retweets"] as? Int ?? 0
+
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        if let replyingTo = dictionary["replyingTo"] as? String {
+            self.replyingTo = replyingTo
+        }
+    }
+}
+
